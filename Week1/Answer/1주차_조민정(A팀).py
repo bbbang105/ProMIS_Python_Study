eef0d6764d64948a7e5cdecf672321d3f7e60e5b
bbbang105 <2018111366@dgu.ac.kr>
2023-04-12 01:23:48 +0900

    MOD FOLD LOC

---
+++ Week1/Answer/1주차_조민정(A팀).py
@@ -0,0 +1,4 @@
+sen = 'i lovee promis'
+a=sen[:6].upper()
+b=sen[11:].upper()
+print('유다현: '+a+" Pro"+b)
---
+++ Week1/Answer/1주차_한지성(A팀).py
@@ -0,0 +1,8 @@
+sen = 'i lovee promis'
+tmp = sen[11:]
+tmp = tmp.upper()
+sen = sen[:6]+sen[7:11]
+sen = sen.title()
+sen = sen + tmp
+
+print('유다현: '+sen)
